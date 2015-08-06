6fbc3cd865d69ea9e74277ff47f07e2d923b9468
Alex-Curran <alexcurran839@gmail.com>
2015-08-06 10:34:05 -0500

    Started Maze Class

---
+++ MazeGenerator/Maze.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace WorldGen
+{
+    public class Maze
+    {
+        private char[,] _grid;
+        private int _height;
+        private int _width;
+
+        public Maze(int height, int width)
+        {
+            _height = height;
+            _width = width;
+            _grid = new char[height, width];
+
+            Intialize();
+        }
+
+        private void Intialize()
+        {
+            for (var i = 0; i < _grid.GetLength(0); i++)
+            {
+                for (var j = 0; j < _grid.GetLength(1); j++)
+                {
+                    _grid[i, j] = 'X';
+                    Console.Write(_grid[i, j]);
+                }
+                Console.Write('\n');
+            }
+        }
+
+        public void Generate(int x, int y)
+        {
+
+        }
+    }
+
+}
